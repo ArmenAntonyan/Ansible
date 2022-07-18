8029d70c37ae040fe1e586b1b2accf1cd5ebdcc5
armen <armen.ant@gmail.com>
2022-07-18 13:36:23 +0400

    im nkar

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
     <title>Awesome app</title>
   </head>
   <body>
-  <h1>Welcome to your awesome app !</h1>
+  
+ <font color="gold"> <h1>Welcome to your awesome app !</h1>
 
   <p> App backend <?=gethostname()?> </p>
 
-    <img src="http://www.fillmurray.com/408/287" />
+    <img src="https://www.facebook.com/photo.php?fbid=4571429296252726&set=pb.100001571019416.-2207520000..&type=3" />
   </body>
 </html>
