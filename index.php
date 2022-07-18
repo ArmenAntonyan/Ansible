d1017b30c3d0a0a182a79143578ccc5febe255ab
armen <armen.ant@gmail.com>
2022-07-18 14:17:41 +0400

    fb

--- index.php
+++ index.php
@@ -9,6 +9,6 @@
 
   <p> App backend <?=gethostname()?> </p>
 
-    <img src="~/Downloads/111.jpg"/>
+    <img src="https://scontent-sof1-1.xx.fbcdn.net/v/t39.30808-6/253218234_4571429279586061_703360018996626058_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Xucm0BKlsQAAX97CUXj&_nc_ht=scontent-sof1-1.xx&oh=00_AT9VfzImLwTWkRMGwRv2jJQxYlIfrPjbgy8rwL-0NuKxEg&oe=62DA34BE"/>
   </body>
 </html>
